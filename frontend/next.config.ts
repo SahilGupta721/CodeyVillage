67a487b28910fefa858d80abeb56383e348c9600
Sahil Gupta <sahilgupta70500@gmail.com>
2026-05-10 12:07:37 -0400

    Recovered earlier version for CORS fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/ws/:path*',
+        destination: 'wss://productivity-island-backend-whqijrostq-uc.a.run.app/ws/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
